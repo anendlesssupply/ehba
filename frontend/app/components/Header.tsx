088dcff31bbb040ad0cb60ae0b7a8fd5feff72d4
anendlesssupply <anendlesssupply@gmail.com>
2025-10-28 21:47:50 +0000

    update dark mode:

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link'
 import {settingsQuery} from '@/sanity/lib/queries'
 import {sanityFetch} from '@/sanity/lib/live'
 import MenuItems from './MenuItems'
-import IconLogo from './IconLogo'
 
 export default async function Header() {
   const {data: settings} = await sanityFetch({
@@ -10,7 +8,7 @@ export default async function Header() {
   })
 
   return (
-    <header className="px-4 pt-8 pb-8 lg:pt-12 lg:px-10 sticky top-0 z-50 bg-white lg:bg-none">
+    <header className="px-4 pt-8 pb-8 lg:pt-12 lg:px-10 sticky top-0 z-50 bg-white dark:bg-black lg:bg-none">
       <MenuItems title={settings?.title} menuItems={settings?.menuItems || []} />
     </header>
   )
